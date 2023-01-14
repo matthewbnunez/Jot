0f27b95e92c97b4c623036dbc23b96b2b255526f
Matthew Nunez <matthew.brandon.nunez@gmail.com>
2023-01-14 07:41:00 -0800

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 8000;
+require('dotenv').config();
+
+// config mongoose
+require('./config/mongoose.config');
+
+//config express
+app.use(cors( {
+    credentials: true,
+    origin:'http://localhost:3000'
+}))
+
+//POST method
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+// require('./server/routes/user.routes')(app);
+
+//port
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
